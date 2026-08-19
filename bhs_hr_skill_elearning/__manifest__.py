c38096cbc117d7f786d54a7b85ef7404a0cf5045
“YenTran” <yentran.bhtech@gmail.com>
2026-08-19 17:51:56 +0700

    v18: update image for index

--- bhs_hr_skill_elearning/__manifest__.py
+++ bhs_hr_skill_elearning/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'HR Skill eLearning',
+    'name': 'Employee Learning Monitor',
     'author': 'Bac Ha Software',
     'website': 'https://bachasoftware.com',
     'maintainer': 'Bac Ha Software',
